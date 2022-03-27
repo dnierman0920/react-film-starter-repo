2432d164d26d25646c0e4df7cb76c56e95e781c3
dnierman0920 <davidnierman93@gmail.com>
2022-03-27 08:39:06 -0700

    Make Fave component change when movie is favorited

--- src/Fave.js
+++ src/Fave.js
@@ -7,23 +7,37 @@ export default class Fave extends Component{
 
         // This binding is necessary to make `this` work in the callback
         this.handleClick = this.handleClick.bind(this);
+
+        this.state = {
+            isFave: false
+        }
+    }
+
+    state = {
+        isFave: false
     }
 
     handleClick = (e) => {
         e.stopPropagation()
-        console.log("handling Fave click!")
-        console.log("Event: ", e)
+        // console.log("handling Fave click!")
+        // console.log("Event: ", e)
+        this.setState({
+            isFave:!this.state.isFave
+        })
     }
 
 
     render() {
+        
+        const action = this.state.isFave ? 'remove_from_queu' : 'add_to_queue'
+
         return(
-            <div className="film-row-fave add_to_queue">
+            <div className={`film-row-fave ${action}`}>
                 <p
                     className="material-icons"
                     onClick={(e) => {this.handleClick(e)}}
                 >
-                    add_to_queue 
+                    {action}
                 </p>
             </div>
         )
